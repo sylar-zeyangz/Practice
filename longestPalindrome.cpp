39c7fe9560a88a44c53827f5ec96a1d6a2c9a8f8
sylar <sylarz@mail.com>
2020-02-09 15:14:34 -0800

    Create longestPalindrome.cpp

---
+++ longestPalindrome.cpp
@@ -0,0 +1,39 @@
+#include<unordered_map> 
+#include<vector>
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.size() <= 1) 
+            return s;
+        int min_start = 0, max_len = 1;
+        for (int i = 0; i < s.size();) {
+            if (s.size() - i <= max_len / 2) 
+                break;
+            int j = i, k = i;
+            while (k < s.size()-1 && s[k+1] == s[k]) 
+                ++k; // Skip duplicate characters.
+            i = k+1;
+            while (k < s.size()-1 && j > 0 && s[k + 1] == s[j - 1]) { 
+                ++k; 
+                --j;
+            } // Expand.
+            int new_len = k - j + 1;
+            if (new_len > max_len) { 
+                min_start = j; 
+                max_len = new_len;
+            }
+        }
+    return s.substr(min_start, max_len);
+}
+};
+
+int main()
+{
+    Solution S;
+    string s = "abcdasdfghjkldcba";
+    cout<<"The answer is: "<< S.longestPalindrome(s) << endl;
+    return 0;
+}
