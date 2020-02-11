ffca129bfa0abc682574353a66524fafaf2de058
sylar <sylarz@mail.com>
2020-02-11 15:22:52 -0800

    Create longestCommonPrefix.cpp

---
+++ longestCommonPrefix.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() <= 0) return "";
+        string lstring = strs[0];
+        for (int i = 1; i<strs.size(); i++) {
+            for (int j = 0; j < lstring.size(); j++) {
+                if (lstring[j] != strs[i][j])
+                    lstring = lstring.substr(0, j);
+            }
+        }
+        return lstring;
+    }
+};
+int main()
+{
+    Solution S;
+    vector<string> s;
+    cout<<"The answer is: "<< S.longestCommonPrefix(s) << endl;
+    return 0;
+}
