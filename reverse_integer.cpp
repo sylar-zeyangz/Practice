2fec8f1f78f6f8cbe403e3160de77678f8bc8cc1
sylar <sylarz@mail.com>
2020-02-10 21:58:57 -0800

    Create reverse_integer.cpp

---
+++ reverse_integer.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+class Solution {
+public:
+    int reverse(int x) {
+        int ret = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if ( ret < INT_MIN/10 || ret > INT_MAX/10 || 
+                 (ret == INT_MAX/10 && pop > 7) || 
+                 (ret == INT_MIN/10 && pop < -8 )) return 0;
+            ret = ret * 10 + pop;
+        }
+        return ret;
+    }
+};
+
+int main()
+{
+    int x = 1463847412;
+    Solution S;
+    cout<<"The answer is: "<< S.reverse(x) << endl;
+    return 0;
+}
