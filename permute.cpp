0c22d7c8584db7cf3c8313117c7e42b14605304e
sylar <sylarz@mail.com>
2020-02-21 14:00:02 -0800

    Update permute.cpp

--- permute.cpp
+++ permute.cpp
@@ -10,21 +10,28 @@ public:
         vector<int> curr;
         vector<bool> used(nums.size(), false);
         // dfs for permutation n = 3 (as in choosing 3 nums from vector<int> nums)
-        permutation(nums, 0, nums.size(), used, ans, curr);
+        permutation(nums, used, ans, curr);
         return ans;
     }
     
 private:
-    void permutation(vector<int>& nums, int d, int n, vector<bool>& used, vector<vector<int>>& ans, vector<int>& curr) {
-        if (d == n) {
+    void permutation(vector<int>& nums, vector<bool>& used, vector<vector<int>>& ans, vector<int>& curr) {
+        if (curr.size() == nums.size()) {
+            // Add the following line if the nums has duplications and permutation needs to be unique.
+            // Solution 1: (This operation will cost emnormous time)
+            // if(find(ans.begin(), ans.end(), curr) == ans.end())
             ans.push_back(curr);
             return;
         }
         for (int i = 0; i < nums.size(); i++) {
             if (used[i]) continue;
+            // Add the following line if the nums has duplications and permutation needs to be unique.
+            // Solution 2: (Better than Solution 1) 
+            // Same number can be only used once at each depth.
+            // if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
             used[i] = true;
             curr.push_back(nums[i]);
-            permutation(nums, d+1, n, used, ans, curr);
+            permutation(nums, used, ans, curr);
             curr.pop_back();
             used[i] = false;
         }
