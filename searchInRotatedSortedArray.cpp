f9100f3a43784e8f14c5ba8b1f20faa37cab8322
sylar <sylarz@mail.com>
2020-07-08 15:26:41 -0700

    Update searchInRotatedSortedArray.cpp

--- searchInRotatedSortedArray.cpp
+++ searchInRotatedSortedArray.cpp
@@ -15,7 +15,7 @@ public:
     int findPeak(vector<int> nums) { 
         int l = 0, r = nums.size() - 1;
         while (l <= r) {
-            int mid = l + (r - l) / 2;
+            int mid = l + r / 2;
             if (nums[mid] > nums[mid+1])
                 return mid;
             if (nums[mid] < nums[r])
