3e7c53a48dfea91567cc7bb7765c2ea3be6a1800
Sylar Zeyang ZHANG <60757644+sylar-zeyangz@users.noreply.github.com>
2021-09-30 21:01:28 -0700

    Update longestPalindrome.cpp

--- longestPalindrome.cpp
+++ longestPalindrome.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 class Solution {
 public:    
-    string longestPalindrome(string s) {
+    // Better interpretation
+    string longestPalindrome(string s) { 
         const int n = s.length();
         auto getLen = [&](int l, int r)
         {
